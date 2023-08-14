d082285bd661f431edcf82dfc35d57732b206893
Pawan Paudel <pawanpaudel93@gmail.com>
2023-08-14 12:19:32 +0545

    fix: Update the nextauth api

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,6 +3,7 @@ import { JWKInterface } from "arweave/node/lib/wallet";
 import { NextApiRequest, NextApiResponse } from "next";
 import NextAuth, { AuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+import crypto from "crypto";
 
 // For more information on each option (and a full list of options) go to
 // https://next-auth.js.org/configuration/options
@@ -34,13 +35,36 @@ export const authOptions = (req: NextApiRequest): AuthOptions => {
             credentials?.signature &&
             credentials?.publicKey
           ) {
-            const isSignatureValid = await arweave.crypto.verify(
-              credentials?.publicKey,
-              base64StringToUint8Array(credentials?.message),
-              base64StringToUint8Array(credentials?.signature)
+            const message = base64StringToUint8Array(credentials?.message);
+            const signature = base64StringToUint8Array(credentials?.signature);
+            const publicJWK: JsonWebKey = {
+              e: "AQAB",
+              ext: true,
+              kty: "RSA",
+              n: credentials.publicKey,
+            };
+
+            // import public jwk for verification
+            const verificationKey = await crypto.subtle.importKey(
+              "jwk",
+              publicJWK,
+              {
+                name: "RSA-PSS",
+                hash: "SHA-256",
+              },
+              false,
+              ["verify"]
             );
 
-            if (isSignatureValid) {
+            // verify the signature by matching it with the message
+            const isValidSignature = await crypto.subtle.verify(
+              { name: "RSA-PSS", saltLength: 32 },
+              verificationKey,
+              signature,
+              message
+            );
+
+            if (isValidSignature) {
               const address = await arweave.wallets.jwkToAddress({
                 n: credentials.publicKey,
               } as JWKInterface);
